3e8f2ddd13b106a8bdb95ec5ba30523384ec416c
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-10-23 19:55:03 +0300

    Compound Assignment With Augmented Multiplication

--- script.js
+++ script.js
@@ -119,3 +119,13 @@ let c = 3;
 a -= 6;
 b -= 15;
 c -= 1;
+
+// Compound Assignment With Augmented Multiplication
+// Convert the assignments for a, b, and c to use the *= operator.
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
