dbee6eb656e8ba7c4916a561d7e08dd23cd2402e
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-10-23 12:32:06 +0300

    Differences Between the var and let Keywords

--- script.js
+++ script.js
@@ -46,3 +46,8 @@ studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
 
+/* Explore Differences Between the var and let Keywords
+So unlike var, when you use let, a variable with the same name can only be declared once.
+Update the code so it only uses the let keyword. */
+let catName = "Oliver";
+let catSound = "Meow!";
