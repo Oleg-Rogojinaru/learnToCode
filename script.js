8af6de9d9cb01168adf3d6f1b186565e3b650287
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-10-23 20:02:36 +0300

    Compound Assignment With Augmented Division

--- script.js
+++ script.js
@@ -129,3 +129,13 @@ let c = 4.6;
 a *= 5;
 b *= 3;
 c *= 10;
+
+// Compound Assignment With Augmented Division
+// Convert the assignments for a, b, and c to use the /= operator.
+let a = 48;
+let b = 108;
+let c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
