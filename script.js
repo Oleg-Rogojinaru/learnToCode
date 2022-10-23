34f40a6b386a8d204667df3ed40a3503527501e6
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-10-23 11:54:58 +0300

    Define a variable

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ var myName;
 // Assign the value 7 to variable a
 var a;
 a = 7;
+
+// Assign the contents of a to variable b.
+var a;
+a = 7;
+var b;
+b = a;
+
+// Define a variable a with var and initialize it to a value of 9.
+var a = 9;
+
+//
