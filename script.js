cb0eaeefcdc98bd3784467a8e013c33d7e1f6124
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-10-23 15:09:32 +0300

    Multiply Two Decimals with JavaScript

--- script.js
+++ script.js
@@ -87,3 +87,7 @@ myVar--;
 // Create Decimal Numbers with JavaScript
 // Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
 const myDecimal = 5.7;
+
+// Multiply Two Decimals with JavaScript
+// Change the 0.0 so that product will equal 5.0.
+const product = 2.0 * 2.5;
