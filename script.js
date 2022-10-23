e9ebcea8b797a7c3ea586fd56b60e779bdb29a49
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-10-23 20:34:24 +0300

    Quoting Strings with Single Quotes

--- script.js
+++ script.js
@@ -145,3 +145,9 @@ c /= 11;
 it to the console, you would see:
 I am a "double quoted" string inside "double quotes". */
 const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+// Quoting Strings with Single Quotes
+/* Change the provided string to a string with single quotes at the beginning and end 
+and no escape characters.
+const myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>"; */
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
