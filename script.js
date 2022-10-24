32e2da0cab8a948bb5c776f9661b836a635317a5
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-10-24 19:25:42 +0300

    Concatenating Strings with Plus Operator

--- script.js
+++ script.js
@@ -170,3 +170,8 @@ FirstLine
 ThirdLine
 */
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+// Concatenating Strings with Plus Operator
+/* Build myStr from the strings This is the start. and This is the end. using the + operator. 
+   Be sure to include a space between the two strings. */
+	const myStr = "This is the start." + " This is the end.";
