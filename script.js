36262780fe944e6b1ad5f66c086b32c19b198307
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-10-28 22:15:10 +0300

    Manipulate Arrays With pop()

--- script.js
+++ script.js
@@ -258,3 +258,8 @@ const myArray = [
  // Push ["dog", 3] onto the end of the myArray variable.
  const myArray = [["John", 23], ["cat", 2]];
  myArray.push(["dog", 3]);
+
+ // Manipulate Arrays With pop()
+ // Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
+ const myArray = [["John", 23], ["cat", 2]];
+ const removedFromMyArray = myArray.pop();
