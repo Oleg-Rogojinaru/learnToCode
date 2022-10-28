a39867dbdb3f965d86d1f47a8ec16fb652289b21
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-10-28 22:50:56 +0300

    Write Reusable JavaScript with Functions

--- script.js
+++ script.js
@@ -285,3 +285,12 @@ const myList = [
 	["Cheese", 1],
 	["bread", 3]
  ];
+ 
+// Write Reusable JavaScript with Functions
+// 1. Create a function called reusableFunction which prints the string Hi World to the dev console.
+// 2. Call the function.
+function reusableFunction() {
+	console.log("Hi World");
+ }
+ 
+reusableFunction();
