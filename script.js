08d8ea9733871f24742fcceaa4040bc7311ccd39
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-11-03 22:23:09 +0200

    Understanding Boolean Values

--- script.js
+++ script.js
@@ -413,3 +413,12 @@ function nextInLine(arr, item) {
  console.log("Before: " + JSON.stringify(testArr));
  console.log(nextInLine(testArr, 6));
  console.log("After: " + JSON.stringify(testArr));
+
+
+// Understanding Boolean Values
+// Modify the welcomeToBooleans function so that it returns true instead of false when the run button is clicked.
+function welcomeToBooleans() {
+	// Only change code below this line
+	return true; // Change this line
+ 	// Only change code above this line
+ }
