1295cea3710cd891d38dee0b112c78e820a796ee
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-11-03 14:58:15 +0200

    Passing Values to Functions with Arguments

--- script.js
+++ script.js
@@ -298,3 +298,8 @@ reusableFunction();
 // Passing Values to Functions with Arguments
 // 1. Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
 // 2. Call the function with two numbers as arguments.
+function functionWithArgs(param1, param2) {
+	console.log(param1 + param2); 
+ }
+ 
+ functionWithArgs(4,5);
