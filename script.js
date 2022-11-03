be05b7363574900a1df03421780177e029d4f890
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-11-03 18:42:55 +0200

    Assignment with a Returned Value

--- script.js
+++ script.js
@@ -381,3 +381,15 @@ function addFive() {
 
 addThree();
 addFive();
+
+
+// Assignment with a Returned Value
+// Call the processArg function with an argument of 7 and assign its return value to the variable processed.
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+// Only change code below this line
+processed = processArg(7);
