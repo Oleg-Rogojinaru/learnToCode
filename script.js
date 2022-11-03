655cf8a6007d85910d8b69e7a285f307c4d84229
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-11-03 15:22:59 +0200

    Local Scope and Functions

--- script.js
+++ script.js
@@ -331,3 +331,21 @@ function fun2() {
     output += " oopsGlobal: " + oopsGlobal;
   }
   console.log(output);
+}
+
+
+// Local Scope and Functions
+// Declare a local variable myVar inside myLocalScope and run the tests.
+// Note: The console will still display ReferenceError: myVar is not defined, but this will not cause the tests to fail.
+function myLocalScope() {
+	// Only change code below this line
+	let myVar;
+	console.log('inside myLocalScope', myVar);
+ }
+ myLocalScope();
+ 
+ // Run and check the console
+ // myVar is not defined outside of myLocalScope
+ console.log('outside myLocalScope', myVar);
+
+
