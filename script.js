821057fae70b954ed7b31de59b96e0a0639f6e1d
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-11-04 18:27:26 +0200

    Comparisons with the Logical Or Operator

--- script.js
+++ script.js
@@ -586,3 +586,20 @@ function testLogicalAnd(val) {
  }
  
  testLogicalAnd(10);
+
+
+// Comparisons with the Logical Or Operator
+// Combine the two if statements into one statement which returns the string Outside if val is not between 10 and 20, inclusive.
+// Otherwise, return the string Inside
+function testLogicalOr(val) {
+	// Only change code below this line
+ 
+	if (val < 10 || val > 20) {
+	  return "Outside";
+	}
+ 
+	// Only change code above this line
+	return "Inside";
+ }
+ 
+ testLogicalOr(15);
