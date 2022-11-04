33b856f37dcc6ea71fa8c22e2767d23bc3d728db
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-11-04 17:39:59 +0200

    Comparison with the Equality Operator

--- script.js
+++ script.js
@@ -435,3 +435,16 @@ function trueOrFalse(wasThatTrue) {
 	// Only change code above this line
  }
 
+
+ // Comparison with the Equality Operator
+ // Add the equality operator to the indicated line so that the function will return the string Equal when val is equivalent to 12.
+ // Setup
+function testEqual(val) {
+	if (val == "12") { // Change this line
+	  return "Equal";
+	}
+	return "Not Equal";
+ }
+ 
+ testEqual(10);
+ 
