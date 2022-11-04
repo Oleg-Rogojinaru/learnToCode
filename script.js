fa52fe925a62f0ce57bd0947121ec35795adc4a9
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-11-04 17:49:32 +0200

    Practice comparing different values

--- script.js
+++ script.js
@@ -460,3 +460,17 @@ function testStrict(val) {
  }
  
  testStrict(10);
+
+
+// Practice comparing different values
+// The compareEquality function in the editor compares two values using the equality operator. 
+// Modify the function so that it returns the string Equal only when the values are strictly equal.
+// Setup
+function compareEquality(a, b) {
+	if (a === b) { // Change this line
+	  return "Equal";
+	}
+	return "Not Equal";
+ }
+ 
+ compareEquality(10, "10");
