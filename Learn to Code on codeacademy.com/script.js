4a704b2fba317fd44e42cd8d3a28fca239b3dc43
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-11-05 22:52:36 +0200

    Data Types

--- Learn to Code on codeacademy.com/script.js
+++ Learn to Code on codeacademy.com/script.js
@@ -1,6 +1,17 @@
 
 // Console
-// Use the console.log code in the editor to log your age to the console.
+// 1. Use the console.log code in the editor to log your age to the console.
 console.log(38);
-// On the next line, write another console.log to print out a different number representing the number of weeks you’ve been programming.
+// 2. On the next line, write another console.log to print out a different number representing the number of weeks you’ve been programming.
 console.log(2);
+
+
+// Data Types
+// 1. On line 1, log the string 'JavaScript' to the console.
+console.log('JavaScript');
+// 2. On line 2, log the number 2011 to the console.
+console.log(2011);
+// 3. On line 3, print 'Woohoo! I love to code! #codecademy' to the console.
+console.log('Woohoo! I love to code! #codecademy');
+// 4. On line 4, print the number 20.49 to the console.
+console.log(20.49);
