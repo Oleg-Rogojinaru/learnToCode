900de62f1740752b674c6c7e79eae03fdf60251b
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-11-05 15:35:41 +0200

    Logical Order in If Else Statements

--- script.js
+++ script.js
@@ -637,3 +637,18 @@ function testElseIf(val) {
  }
  
  testElseIf(7);
+
+
+// Logical Order in If Else Statements
+// Change the order of logic in the function so that it will return the correct statements in all cases.
+function orderMyLogic(val) {
+	if  (val < 5) {
+	  return "Less than 5";
+	} else if (val < 10) {
+	  return "Less than 10";
+	} else {
+	  return "Greater than or equal to 10";
+	}
+ }
+ 
+ orderMyLogic(7);
