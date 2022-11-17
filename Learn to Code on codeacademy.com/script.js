e958f46e2acca07ed5cd0e41078c4cc814136fef
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-11-17 16:05:38 +0200

    String Interpolation

--- Learn to Code on codeacademy.com/script.js
+++ Learn to Code on codeacademy.com/script.js
@@ -190,3 +190,20 @@ let favoriteAnimal = 'Raccoon';
 		Use string concatenation so that ANIMAL is replaced with the value in your favoriteAnimal variable.
 */
 console.log('My favorite animal: ' + favoriteAnimal);
+
+
+
+
+// String Interpolation
+// 1. Create a variable called myName and assign it your name.
+let myName = 'Oleg';
+
+// 2. Create a variable called myCity and assign it your favorite city’s name.
+let myCity = 'Stockholm';
+
+/* 3. Use a single template literal to interpolate your variables into the sentence below. 
+		Use console.log() to print your sentence to the console in the following format:
+		My name is NAME. My favorite city is CITY.
+		Replace NAME and CITY in the string above by interpolating the values saved to myName and myCity.
+*/
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
