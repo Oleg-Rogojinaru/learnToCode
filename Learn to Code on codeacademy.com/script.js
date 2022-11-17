2f7b460057a1d423ebacdd4dda899ebafd82e728
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-11-17 11:36:17 +0200

    Create a Variable: const

--- Learn to Code on codeacademy.com/script.js
+++ Learn to Code on codeacademy.com/script.js
@@ -131,3 +131,13 @@ let changeMe = true;
 */
 changeMe = false;
 console.log(changeMe);
+
+
+
+
+// Create a Variable: const
+// 1. Create a constant variable named entree and set it to equal to the string 'Enchiladas'.
+const entree = 'Enchiladas';
+
+// 2. Just to check that you’ve saved the value of 'Enchiladas' to entree, log the value of entree to the console.
+console.log(entree);
