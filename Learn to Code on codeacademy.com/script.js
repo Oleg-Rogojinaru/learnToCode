feb52627b6c65af2c4b1739d3f9cbb82c7e757cb
Oleg Rogojinaru <olegrogojinaru@gmail.com>
2022-11-17 22:43:26 +0200

    typeof operator

--- Learn to Code on codeacademy.com/script.js
+++ Learn to Code on codeacademy.com/script.js
@@ -207,3 +207,21 @@ let myCity = 'Stockholm';
 		Replace NAME and CITY in the string above by interpolating the values saved to myName and myCity.
 */
 console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+
+
+
+// typeof operator
+// 1. Use console.log() to print the typeof newVariable.
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+
+/* 2. Great, now let’s check what happens if we reassign the variable.
+		Below the console.log() statement, reassign newVariable to 1.
+*/
+newVariable = 1;
+
+/* 3. Since you assigned this new value to newVariable, it has a new type! 
+		On the line below your reassignment, use console.log() to print typeof newVariable again.
+*/
+console.log(typeof newVariable);
